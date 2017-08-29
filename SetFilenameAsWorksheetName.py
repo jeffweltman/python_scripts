87bc46b8e319e1f8258d561eb39354eaa03d13fc
jeffweltman <31448076+jeffweltman@users.noreply.github.com>
2017-08-29 11:20:42 -0500

    Add files via upload

---
+++ SetFilenameAsWorksheetName.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+#SetFilenameAsWorksheetName.py
+#created by Jeff Weltman
+#v1.0 8/29/2017
+
+def main():
+    import os
+    import openpyxl
+    target_dir = ('C:\\Users\\46669510\\Documents\\PythonTest') # set target directory
+    pathiter = (os.path.join(root, filename)
+        for root, _, filenames in os.walk(target_dir)
+        for filename in filenames
+    )
+    for path in pathiter:
+        filename = path.lstrip(target_dir) # this gets the name and extension of the current workbook
+        book = filename.rstrip('.xlsx') # this strips the extension
+        #print(book) 
+        dollar = '$'
+        newtitle = book + dollar
+        badname = 'Workheet for {} was not correct.'.format(book)
+        goodname = 'Workheet for {} is correct.'.format(book)
+        from openpyxl import load_workbook
+        wb = load_workbook(path)
+        ws = wb.active
+        #print(ws)
+        #print(wb.sheetnames)
+        for sheet in wb:
+            if ws.title != newtitle:
+                ws.title = newtitle
+                print(badname)
+            else:
+                print(goodname)
+                continue
+        wb.save(path)
+
+if __name__ == "__main__": main()
