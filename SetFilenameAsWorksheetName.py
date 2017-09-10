7c40679c9ef265482dcc0bdbdbbba8a940cbb811
Jeff Weltman <31448076+jeffweltman@users.noreply.github.com>
2017-09-10 15:35:26 -0500

    Update SetFilenameAsWorksheetName.py

--- SetFilenameAsWorksheetName.py
+++ SetFilenameAsWorksheetName.py
@@ -1,4 +1,4 @@
-s#!/usr/bin/python3
+#!/usr/bin/python3
 #SetFilenameAsWorksheetName.py
 #created by Jeff Weltman
 #https://github.com/jeffweltman/python_scripts
@@ -7,29 +7,26 @@ s#!/usr/bin/python3
 def main():
     import os
     import openpyxl
-    target_dir = ('C:\\Users\\administrator\\Documents\\PythonTest') # set target directory
-    pathiter = (os.path.join(root, filename)
-        for root, _, filenames in os.walk(target_dir)
-        for filename in filenames
-    )
-    for path in pathiter:
-        filename = path.lstrip(target_dir) # this gets the name and extension of the current workbook
-        book = filename.rstrip('.xlsx') # this strips the extension
-        #print(book) 
-        badname = 'Worksheet for {} was not correct.'.format(book)
-        goodname = 'Worksheet for {} is correct.'.format(book)
-        from openpyxl import load_workbook
-        wb = load_workbook(path)
-        ws = wb.active
-        #print(ws)
-        #print(wb.sheetnames)
-        for sheet in wb:
-            if ws.title != book:
-                ws.title = book
-                print(badname)
-            else:
-                print(goodname)
-                continue
-        wb.save(path)
+    target_dir = ('C:\\users\jeffw\Documents') # set target directory
+    for file in os.listdir(target_dir):
+        filename = os.fsdecode(file)
+        if filename.endswith('.xlsx'): #looking only at Excel docs
+            book = filename.rstrip('.xlsx') # this strips the extension
+            #print(book) 
+            badname = 'Worksheet for {} was not correct.'.format(book)
+            goodname = 'Worksheet for {} is correct.'.format(book)
+            from openpyxl import load_workbook
+            wb = load_workbook(filename)
+            ws = wb.active
+            #print(ws)
+            #print(wb.sheetnames)
+            for sheet in wb:
+                if ws.title != book:
+                    ws.title = book
+                    print(badname)
+                else:
+                  print(goodname)
+                  continue
+                wb.save(filename)
 
 if __name__ == "__main__": main()
