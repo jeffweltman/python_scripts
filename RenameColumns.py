c29ae63251407919f35ec7ac20e12cdfe1516e20
Jeff Weltman <31448076+jeffweltman@users.noreply.github.com>
2017-09-10 15:39:01 -0500

    Update RenameColumns.py

--- RenameColumns.py
+++ RenameColumns.py
@@ -7,27 +7,24 @@
 def main():
     import os
     import openpyxl
-    target_dir = ('C:\\Users\\administrator\\Documents\\PythonTest') # set target directory
-    pathiter = (os.path.join(root, filename)
-        for root, _, filenames in os.walk(target_dir)
-        for filename in filenames
-    )
-    for path in pathiter:
-        filename = path.lstrip(target_dir) # this gets the name and extension of the current workbook
-        book = filename.rstrip('.xlsx') # this strips the extension
-        print(book)
-        from openpyxl import load_workbook
-        wb = load_workbook(path)
-        ws = wb.active
-        for row in ws.iter_rows(min_row=1, max_col=75, max_row=1): #setting only the first row to get the headers
-            for cellObj in row:
-                print(cellObj.value)
-                if cellObj.value == 'sem_exam': #search term for inaccurate column name
-                    cellObj.value = 'n_sem_exam' #replace search term with this target column name
-                elif cellObj.value == 'sem_final':
-                    cellObj.value = 'n_sem_final'
-                elif not cellObj.value: break #break after the first null column name if max_col number above exceeds existing column number
-                else: continue
-        wb.save(path)
+    target_dir = ('C:\\Users\\jeffw\\Documents\\') # set target directory
+    for file in os.listdir(target_dir):
+        filename = os.fsdecode(file)
+        if filename.endswith('.xlsx'): #looking only at Excel docs
+            book = filename.rstrip('.xlsx') # this strips the extension
+            print(book)
+            from openpyxl import load_workbook
+            wb = load_workbook(filename)
+            ws = wb.active
+            for row in ws.iter_rows(min_row=1, max_col=75, max_row=1): #setting only the first row to get the headers
+                for cellObj in row:
+                    print(cellObj.value)
+                    if cellObj.value == 'sem_exam': #search term for inaccurate column name
+                        cellObj.value = 'n_sem_exam' #replace search term with this target column name
+                    elif cellObj.value == 'sem_final':
+                        cellObj.value = 'n_sem_final'
+                    elif not cellObj.value: break #break after the first null column name if max_col number above exceeds existing column number
+                    else: continue
+                wb.save(filename)
         
 if __name__ == "__main__": main()
