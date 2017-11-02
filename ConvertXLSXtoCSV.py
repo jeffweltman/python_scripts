6108ea2c074e3e9bcae2b9842c7983431c21175f
Jeff Weltman <31448076+jeffweltman@users.noreply.github.com>
2017-11-02 09:29:24 -0500

    Update ConvertXLSXtoCSV.py
    Commenting.

--- ConvertXLSXtoCSV.py
+++ ConvertXLSXtoCSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 #ConvertXLSXtoCSV.py
-#created by Jeff Weltman
+#Created by Jeff Weltman
 #https://github.com/jeffweltman/python_scripts
-#v1.0 10/30/2017
+#v1.1 11/02/2017
 #Requires openpyxl package
 
 def main():
@@ -12,22 +12,18 @@ def main():
     target_dir = ('Documents') # set target directory
     for file in os.listdir(target_dir):
         filename = os.fsdecode(file)
-        if filename.endswith('.xlsx'): #looking only at Excel docs
+        if filename.endswith('.xlsx'): #looking only at Excel xlsx docs
             book = filename.rstrip('.xlsx') # this strips the extension
             fullpath = target_dir + "/" + filename
-            #print(book) 
             success = '{} converted to CSV.'.format(book)
             from openpyxl import load_workbook
             wb = load_workbook(fullpath)
             sh = wb.get_active_sheet()
-            #print(ws)
-            #print(sh.sheetnames)
             with open(fullpath.rstrip('.xlsx') + '.csv', 'w') as f:
                 c = csv.writer(f)
-                for r in sh.rows:
-                # here, you can also filter non-ascii characters
+                for r in sh.rows:                
                     c.writerow([cell.value for cell in r])
-                print(success)    
+                print(success) # prints successful conversions to console for each file   
                 f.close()
 
 if __name__ == "__main__": main()
